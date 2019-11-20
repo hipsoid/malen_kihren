6aefba76dab85f9a1e173edfb4a7ed8845e6e6f2
hipsoid <artyom.agafonov@gmail.com>
2019-11-20 15:04:12 +0200

    Убрал очень много

--- handlers.go
+++ handlers.go
@@ -11,9 +11,6 @@ import (
 
 )
 
-var heads = []string{"beluga", "bendr", "dead", "evil", "fang", "pixel", "regular", "safe", "sand-worm", "shades", "silly", "smile", "tongue"}
-var tails = []string{"block-bum", "bolt", "curled", "fat-rattle", "freckled", "hook", "pixel", "regular", "round-bum", "sharp", "skinny", "small-rattle"}
-
 
 func str(str string) *string {
 	return &str
@@ -28,32 +25,7 @@ func end(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Game end - %v\n", string(body))
 }
 
-func start(w http.ResponseWriter, r *http.Request) {
-	var requestData GameStartRequest
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	log.Printf("Game starting - %v\n", string(body))
-	err = json.Unmarshal(body, &requestData)
-	if err != nil {
-		log.Println(err)
-		return
-	}
 
-	responseData := GameStartResponse{
-		Name:     "malen_kihren",
-		HeadType: str("smile"),
-		TailType: str("hook"),
-	}
-	b, err := json.Marshal(responseData)
-	if err != nil {
-		log.Println("%v", err)
-		return
-	}
-	w.Write(b)
-}
 
 func pp(val []byte) {
 
@@ -70,16 +42,7 @@ func move(w http.ResponseWriter, r *http.Request) {
 		Move:  requestData.GenerateMove(),
 		Taunt: str(taunts[rand.Intn(len(taunts))]),
 	}
-	log.Printf("Move request - direction:%v - taunt: %v\n", responseData.Move, *responseData.Taunt)
-	if err != nil {
-		fmt.Printf("ERR: %#v\n", err)
-	}
-	log.Printf("%v\n", string(val))
-	b, err := json.Marshal(responseData)
-	if err != nil {
-		log.Fatalf("%v", err)
-		return
-	}
+
 	w.Write(b)
 }
 
